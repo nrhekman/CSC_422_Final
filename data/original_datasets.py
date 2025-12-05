cc6dbe9c0923bb6808ad9ffdb70ca297e2c24a44
Naomi Hekman <144081115+nrhekman@users.noreply.github.com>
2025-12-04 23:08:34 -0800

    Add files via upload

---
+++ data/original_datasets.py
@@ -0,0 +1,5 @@
+import kagglehub
+
+pest_path = kagglehub.dataset_download("rupankarmajumdar/crop-pests-dataset")
+
+healthy_path = kagglehub.dataset_download("mdwaquarazam/agricultural-crops-image-classification")
